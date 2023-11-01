8f645c2a04116085cdc22b424bc66057d6b2a9d6
Georgi Serafimov <g.serafimov@c-trust.eu>
2023-11-01 13:29:32 +0200

    19 - using custom components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.ts',
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {}
