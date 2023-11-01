3679ba728c35604c6aa9cda9b991a863578b6f41
Georgi Serafimov <g.serafimov@c-trust.eu>
2023-11-01 16:51:14 +0200

    26 - string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 12;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
